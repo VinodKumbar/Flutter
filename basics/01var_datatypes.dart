d974fa269603b59ca768f764dd96124dd1be2a44
VinodKumbar <vnkumbar@gmail.com>
2023-11-22 21:41:27 +0530

    dart hello world and variables & data types

---
+++ basics/01var_datatypes.dart
@@ -0,0 +1,27 @@
+main() {
+  // integer example
+  var x = 13; // integer
+  var PI = 3.14; //double
+  double z = 12;
+
+  // print(x + PI + z);   // -> cmd+/
+
+// string example
+  var course = "dart";
+  var duration = "45 days";
+
+  // print(course + " can be done in " + duration);
+
+  // Boolean data types
+
+  var isVerified = true;
+  var payementResponse = "Not yet Credited";
+  // print(payementResponse.isEmpty);
+
+  //array  / list    -> in a programming world we always count number of array elements from 0
+  var courselist = ["java", "dart", "react native"];
+  print(courselist[1]);
+  print(courselist.length);
+  print(courselist.removeLast());
+  print(courselist);
+}
