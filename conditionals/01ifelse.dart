001cdde7304293315d356345b7750bb778043633
VinodKumbar <vnkumbar@gmail.com>
2023-11-26 20:57:50 +0530

    if and else staments

---
+++ conditionals/01ifelse.dart
@@ -0,0 +1,15 @@
+void main() {
+  var temprature = 45;
+
+  if (temprature > 40) {
+    print("Hey it's very HOT outside");
+    // } else {
+    //   print("Hey it's Normal out here");
+  }
+
+  if (temprature > 30) {
+    print("Message for 30 Temprature");
+  } else {
+    print("I know how to write if and else in dart");
+  }
+}
